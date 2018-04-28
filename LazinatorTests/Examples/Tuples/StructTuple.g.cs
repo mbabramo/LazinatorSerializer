c19577c7f4de2dcf0585017c243c0d673af6ecb0
Michael Abramowicz <mbabramo@yahoo.com>
2018-04-28 08:37:48 -0400

    codegen

--- LazinatorTests/Examples/Tuples/StructTuple.g.cs
+++ LazinatorTests/Examples/Tuples/StructTuple.g.cs
@@ -21,7 +21,6 @@ using static Lazinator.Core.LazinatorUtilities;
 using Lazinator.Exceptions;
 using Lazinator.Support;
 using Lazinator.Wrappers;
-using LazinatorTests.Examples;
 
 namespace LazinatorTests.Examples.Tuples
 {
