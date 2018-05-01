22b00a9f8eeea37ea3c598cbff4bc55008935c0b
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-01 08:32:18 -0400

    AttributeConverter + codegen

--- LazinatorAnalyzer/Roslyn/AttributeConverter.cs
+++ LazinatorAnalyzer/Roslyn/AttributeConverter.cs
@@ -15,15 +15,15 @@ namespace LazinatorCodeGen.Roslyn
                 case "LazinatorAttribute":
                     var uniqueID = attributeData.GetAttributeConstructorValueByParameterName("uniqueID");
                     if (!(uniqueID is int))
-                        throw new Exception("LazinatorAttribute uniqueID is improperly formatted");
+                        return null;
                     var version = attributeData.GetAttributeConstructorValueByParameterName("version");
                     var autogenerate = attributeData.GetAttributeConstructorValueByParameterName("autogenerate");
                     if (version != null && autogenerate != null)
                     {
                         if (!(version is int))
-                            throw new Exception("LazinatorAttribute version is improperly formatted");
+                            return null;
                         if (!(autogenerate is bool))
-                            throw new Exception("LazinatorAttribute autogenerate is improperly formatted");
+                            return null;
                         return new CloneLazinatorAttribute((int)uniqueID, (int)version, (bool)autogenerate);
                     }
                     else
@@ -31,25 +31,25 @@ namespace LazinatorCodeGen.Roslyn
                 case "EliminatedWithVersionAttribute":
                     var version2 = attributeData.GetAttributeConstructorValueByParameterName("version");
                     if (!(version2 is int))
-                        throw new Exception("CloneEliminatedWithVersionAttribute version is improperly formatted");
+                        return null;
                     return new CloneEliminatedWithVersionAttribute((int) version2);
                 case "IntroducedWithVersionAttribute":
                     var version3 = attributeData.GetAttributeConstructorValueByParameterName("version");
                     if (!(version3 is int))
-                        throw new Exception("IntroducedWithVersionAttribute version is improperly formatted");
+                        return null;
                     return new CloneIntroducedWithVersionAttribute((int)version3);
                 case "SetterAccessibilityAttribute":
                     var accessibility = attributeData.GetAttributeConstructorValueByParameterName("accessibility");
                     if (!(accessibility is string))
-                        throw new Exception("SetterAccessibilityAttribute accessibility is improperly formatted");
+                        return null;
                     return new CloneSetterAccessibilityAttribute((string)accessibility);
                 case "UnofficiallyIncorporateInterfaceAttribute":
                     var otherInterfaceFullyQualifiedTypeName = attributeData.GetAttributeConstructorValueByParameterName("otherInterfaceFullyQualifiedTypeName");
                     if (!(otherInterfaceFullyQualifiedTypeName is string))
-                        throw new Exception("UnofficiallyIncorporateInterfaceAttribute otherInterfaceType is improperly formatted");
+                        return null;
                     var accessibility2 = attributeData.GetAttributeConstructorValueByParameterName("accessibility");
                     if (!(accessibility2 is string))
-                        throw new Exception("UnofficiallyIncorporateInterfaceAttribute accessibility is improperly formatted");
+                        return null;
                     return new CloneUnofficiallyIncorporateInterfaceAttribute((string) otherInterfaceFullyQualifiedTypeName, (string)accessibility2);
                 case "ExcludableChildAttribute":
                     return new CloneExcludableChildAttribute();
