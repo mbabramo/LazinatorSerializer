304b972e8023e43f35358dc8922cf13f911a5f1d
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-02 08:09:40 -0400

    added AVL tree

---
+++ LazinatorTests/Tests/AVL/AvlNodeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lazinator.Collections.AVL;
+
+namespace LazinatorTests.AVL
+{
+	public static class AvlNodeExtensions
+	{
+		public static int Count<TKey>(this AvlNode<TKey, TKey> source)
+		{
+			int count = 1;
+
+			AvlNode<TKey, TKey> left = source.Left;
+			AvlNode<TKey, TKey> right = source.Right;
+
+			if (right != null)
+			{
+				count += Count(left);
+			}
+
+			if (right != null)
+			{
+				count += Count(right);
+			}
+
+			return count;
+		}
+	}
+}
