bea8783c7923d1b26ddc20f807e36ca13230a353
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-03 17:27:10 -0400

    CreateAndSerializeTree in progress

--- Lazinator/Collections/AvlTree/AvlNode.cs
+++ Lazinator/Collections/AvlTree/AvlNode.cs
@@ -8,7 +8,25 @@ using Lazinator.Core;
 namespace Lazinator.Collections.Avl
 {
 	public sealed partial class AvlNode<TKey, TValue> : IAvlNode<TKey, TValue> where TKey : ILazinator, new() where TValue : ILazinator, new()
-    {
-		public AvlNode<TKey, TValue> Parent { get; set; }
+	{
+        // We can't serialize the Parent, so we use the Lazinator built-in parent as a substitute.
+
+	    private AvlNode<TKey, TValue> _Parent;
+	    public AvlNode<TKey, TValue> Parent
+	    {
+	        get
+	        {
+	            if (_Parent == null)
+	            {
+                    if (LazinatorParentClass is AvlTree<TKey, TValue> p)
+	                    _Parent = p.Root;
+                    else
+                        _Parent = (AvlNode<TKey, TValue>)LazinatorParentClass;
+                }
+
+	            return _Parent;
+	        }
+	        set { _Parent = value; }
+	    }
     }
 }
---
+++ LazinatorTests/Tests/AVL/AvlSerializationTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Lazinator.Collections.Avl;
+using Lazinator.Core;
+using Lazinator.Wrappers;
+using Xunit;
+
+namespace LazinatorTests.Tests.AVL
+{
+    public class AvlSerializationTest
+    {
+        private Random r = new Random();
+
+        [Fact]
+        public void CreateAndSerializeTree()
+        {
+            AvlTree<LazinatorWrapperInt, LazinatorWrapperInt> tree = new AvlTree<LazinatorWrapperInt, LazinatorWrapperInt>();
+            tree.DeserializationFactory = new DeserializationFactory(new Type[] { typeof(AvlTree<,>) }, true);
+            Dictionary<int, int> items = new Dictionary<int, int>();
+            for (int i = 0; i < 100; i++)
+            {
+                int k;
+                do
+                {
+                    k = r.Next();
+                } while (items.ContainsKey(k));
+                int v = r.Next();
+                items.Add(k, v);
+                tree.Insert(k, v);
+            }
+
+            var enumerated = tree.Select(x => x.Value.Value).ToList();
+            AvlTree<LazinatorWrapperInt, LazinatorWrapperInt> clone = tree.CloneLazinatorTyped();
+            var enumerated2 = clone.Select(x => x.Value.Value).ToList();
+            var correctOrder = items.OrderBy(x => x.Key).Select(x => x.Value).ToList();
+            enumerated.SequenceEqual(correctOrder).Should().BeTrue();
+            enumerated2.SequenceEqual(correctOrder).Should().BeTrue();
+        }
+    }
+}
