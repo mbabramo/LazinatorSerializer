c40d9a5aabb6650dc62b53f692a8d0b66981b242
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-06 09:06:16 -0400

    in progress

---
+++ Lazinator/Collections/AvlTree/AvlMultiset.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Lazinator.Core;
+
+//namespace Lazinator.Collections.AvlTree
+//{
+//    public class AvlMultiset<T> where T : ILazinator, new()
+//    {
+//        public AvlSet(IComparer<TKey> comparer) : base(comparer)
+//        {
+//        }
+
+//        public AvlSet() : base()
+//        {
+
+//        }
+//    }
+//}
