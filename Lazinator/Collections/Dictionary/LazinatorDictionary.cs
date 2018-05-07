04c376d788442c78a823098579f814dc05566911
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-07 17:52:52 -0400

    codegen

--- Lazinator/Collections/Dictionary/LazinatorDictionary.cs
+++ Lazinator/Collections/Dictionary/LazinatorDictionary.cs
@@ -7,8 +7,6 @@ namespace Lazinator.Collections.Dictionary
 {
     public partial class LazinatorDictionary<TKey, TValue> : ILazinatorDictionary<TKey, TValue> where TKey : ILazinator where TValue : ILazinator
     {
-        public LazinatorList<DictionaryBucket<TKey, TValue>> Buckets { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public LazinatorList<TKey> Keys { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public LazinatorList<TValue> Values { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
     }
 }
