229255d6529e822367e38b310ce042ff22d5f3f2
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-09 06:49:42 -0400

    added children to example

--- LazinatorTests/Examples/Hierarchy/IExample.cs
+++ LazinatorTests/Examples/Hierarchy/IExample.cs
@@ -6,32 +6,29 @@ using LazinatorTests.Examples;
 
 namespace LazinatorTests.Examples
 {
-    [Lazinator((int)ExampleUniqueIDs.Example, 3, true)]
+    [Lazinator((int) ExampleUniqueIDs.Example, 3, true)]
     public interface IExample : ILazinator // note that this ILazinator reference is unnecessary
     {
         bool MyBool { get; set; }
         char MyChar { get; set; }
         ExampleChild MyChild1 { get; set; }
         ExampleChild MyChild2 { get; set; }
-        [EliminatedWithVersion(3)]
-        ExampleChild MyChild2Previous { get; set; }
+        [EliminatedWithVersion(3)] ExampleChild MyChild2Previous { get; set; }
         DateTime MyDateTime { get; set; }
         IExampleNonexclusiveInterface MyInterfaceImplementer { get; set; }
-        [IntroducedWithVersion(3)]
-        string MyNewString { get; set; }
+        [IntroducedWithVersion(3)] string MyNewString { get; set; }
         NonLazinatorClass MyNonLazinatorChild { get; set; }
         bool MyNonLazinatorChild_Dirty { get; set; }
-        [SetterAccessibility("internal")]
-        decimal? MyNullableDecimal { get; }
-        [DerivationKeyword("virtual")]
-        double? MyNullableDouble { get; set; }
+        [SetterAccessibility("internal")] decimal? MyNullableDecimal { get; }
+        [DerivationKeyword("virtual")] double? MyNullableDouble { get; set; }
         TimeSpan? MyNullableTimeSpan { get; set; }
-        [EliminatedWithVersion(3)]
-        string MyOldString { get; set; }
+        [EliminatedWithVersion(3)] string MyOldString { get; set; }
         string MyString { get; set; }
         uint MyUint { get; set; }
         TestEnum MyTestEnum { get; set; }
         TestEnumByte? MyTestEnumByteNullable { get; set; }
         LazinatorWrapperInt WrappedInt { get; set; }
+        [ExcludableChild] ExampleChild ExcludableChild { get; set; }
+        [IncludableChild] ExampleChild IncludableChild { get; set; }
     }
 }
