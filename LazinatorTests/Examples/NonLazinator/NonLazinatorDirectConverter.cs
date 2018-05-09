3306388e6d43f21af59f93bc3d6f08934cd6bf7d
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-09 16:09:41 -0400

    switch to direct converter

---
+++ LazinatorTests/Examples/NonLazinator/NonLazinatorDirectConverter.cs
@@ -0,0 +1,30 @@
+﻿using Lazinator.Buffers;
+using Lazinator.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LazinatorTests.Examples
+{
+    public static class NonLazinatorDirectConverter
+    {
+        public static NonLazinatorClass ConvertFromBytes_LazinatorTests_Examples_NonLazinatorClass(ReadOnlyMemory<byte> storage,
+               DeserializationFactory deserializationFactory, LazinatorUtilities.InformParentOfDirtinessDelegate informParentOfDirtinessDelegate) =>
+               Convert_NonLazinatorType.ConvertFromBytes_LazinatorTests_Examples_NonLazinatorClass(storage, deserializationFactory, informParentOfDirtinessDelegate);
+
+        public static void ConvertToBytes_LazinatorTests_Examples_NonLazinatorClass(BinaryBufferWriter writer,
+            NonLazinatorClass itemToConvert, IncludeChildrenMode includeChildrenMode, bool verifyCleanness) =>
+            Convert_NonLazinatorType.ConvertToBytes_LazinatorTests_Examples_NonLazinatorClass(writer, itemToConvert,
+                includeChildrenMode, verifyCleanness);
+
+
+        public static NonLazinatorStruct ConvertFromBytes_LazinatorTests_Examples_NonLazinatorStruct(ReadOnlyMemory<byte> storage,
+            DeserializationFactory deserializationFactory, LazinatorUtilities.InformParentOfDirtinessDelegate informParentOfDirtinessDelegate) =>
+            Convert_NonLazinatorType.ConvertFromBytes_LazinatorTests_Examples_NonLazinatorStruct(storage, deserializationFactory, informParentOfDirtinessDelegate);
+
+        public static void ConvertToBytes_LazinatorTests_Examples_NonLazinatorStruct(BinaryBufferWriter writer,
+            NonLazinatorStruct itemToConvert, IncludeChildrenMode includeChildrenMode, bool verifyCleanness) =>
+            Convert_NonLazinatorType.ConvertToBytes_LazinatorTests_Examples_NonLazinatorStruct(writer, itemToConvert,
+                includeChildrenMode, verifyCleanness);
+    }
+}
