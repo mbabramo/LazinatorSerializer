803b61869a69f286c1dc4a5ac8a2d33d095daad9
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-09 17:27:29 -0400

    removed debugging code

--- LazinatorAnalyzer/Roslyn/LazinatorCompilation.cs
+++ LazinatorAnalyzer/Roslyn/LazinatorCompilation.cs
@@ -299,10 +299,6 @@ namespace LazinatorCodeGen.Roslyn
 
         private void ConsiderAddingAsRecordLikeType(INamedTypeSymbol type)
         {
-            if (type.Name.Contains("NonLazinatorStruct"))
-            {
-                var DEBUG = 0;
-            }
             if (RecordLikeTypes.ContainsKey(type) || NonRecordLikeTypes.Contains(type))
                 return;
             // Consider whether to add this as a record-like type
