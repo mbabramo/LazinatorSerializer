2e826c553ef294814882f538ba1bad73c28c58db
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-13 16:41:54 -0400

    problem is here: InheritingClosedGeneric codegen doesn't seem to recognize the existence of ClosedGeneric

--- LazinatorTests/Examples/NonAbstractGenerics/InheritingClosedGeneric.cs
+++ LazinatorTests/Examples/NonAbstractGenerics/InheritingClosedGeneric.cs
@@ -4,11 +4,7 @@ using System.Text;
 
 namespace LazinatorTests.Examples.NonAbstractGenerics
 {
-    public class InheritingClosedGeneric : IInheritingClosedGeneric
+    public partial class InheritingClosedGeneric : ClosedGeneric, IInheritingClosedGeneric
     {
-        public int YetAnotherInt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int AnotherPropertyAdded { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public ExampleChild MyT { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public List<ExampleChild> MyListT { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
