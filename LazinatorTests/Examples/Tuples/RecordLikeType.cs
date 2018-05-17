bfcd75af6ff77966de1cf6c93935b79e47bec4d5
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-17 18:27:48 -0400

    record like type with different constructors

--- LazinatorTests/Examples/Tuples/RecordLikeType.cs
+++ LazinatorTests/Examples/Tuples/RecordLikeType.cs
@@ -15,6 +15,21 @@ namespace LazinatorTests.Examples
             this.Name = name;
         }
 
+        public RecordLikeType(int age, string name, string address)
+        {
+            throw new NotImplementedException(); // this constructor should not be called
+        }
+
+        public RecordLikeType(string age, string name)
+        {
+            throw new NotImplementedException(); // this constructor should not be called
+        }
+
+        public RecordLikeType(int age) : this(age, "NoName")
+        {
+
+        }
+
         public RecordLikeType(RecordLikeType other)
         {
             this = other;
