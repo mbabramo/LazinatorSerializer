62f0acafe879a4b1c9773d40f9abe8c269cd6a5f
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-18 07:41:20 -0400

    fix

--- LazinatorTests/Examples/Tuples/RecordTuple.cs
+++ LazinatorTests/Examples/Tuples/RecordTuple.cs
@@ -6,6 +6,5 @@ namespace LazinatorTests.Examples.Tuples
 {
     public partial class RecordTuple : IRecordTuple
     {
-        public MismatchedRecordLikeType MyMismatchedRecordLikeType { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
