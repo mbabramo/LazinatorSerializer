42d494b03b56a534a25ed9d3bd36f7ce4bcadc51
Michael Abramowicz <mbabramo@yahoo.com>
2018-05-24 14:17:07 -0400

    list enum with handcoding

--- LazinatorTests/Examples/Subclasses/IClassWithLocalEnum.cs
+++ LazinatorTests/Examples/Subclasses/IClassWithLocalEnum.cs
@@ -1,8 +1,11 @@
-﻿namespace LazinatorTests.Examples.Subclasses
+﻿using System.Collections.Generic;
+
+namespace LazinatorTests.Examples.Subclasses
 {
     [Lazinator.Attributes.Lazinator((int)ExampleUniqueIDs.ClassWithLocalEnum)]
     public interface IClassWithLocalEnum
     {
         ClassWithLocalEnum.EnumWithinClass MyEnum { get; set; }
+        List<ClassWithLocalEnum.EnumWithinClass> MyEnumList { get; set; }
     }
 }
