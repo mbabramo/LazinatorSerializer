c4523ad008a37ba5b602c0faf3e65d6ff2a7cc2a
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-01 17:48:01 -0400

    codegen

--- LazinatorTests/Examples/Collections/DerivedArray_Values.cs
+++ LazinatorTests/Examples/Collections/DerivedArray_Values.cs
@@ -6,7 +6,5 @@ namespace LazinatorTests.Examples.Collections
 {
     public partial class DerivedArray_Values : Array_Values, IDerivedArray_Values
     { 
-        public int[] MyArrayInt_DerivedLevel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool MyArrayInt_DerivedLevel_Dirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
