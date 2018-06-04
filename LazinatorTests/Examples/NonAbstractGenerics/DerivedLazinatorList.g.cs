3f06bff4c2b9225c98f197db128bc770f91090f7
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-04 11:26:57 -0400

    revised -- not working

--- LazinatorTests/Examples/NonAbstractGenerics/DerivedLazinatorList.g.cs
+++ LazinatorTests/Examples/NonAbstractGenerics/DerivedLazinatorList.g.cs
@@ -98,8 +98,9 @@ namespace LazinatorTests.Examples
         public override int LazinatorObjectVersion { get; set; } = 0;
         
         
-        public virtual void ConvertFromBytesAfterHeader(IncludeChildrenMode includeChildrenMode, int serializedVersionNumber, ref int bytesSoFar)
+        public override void ConvertFromBytesAfterHeader(IncludeChildrenMode includeChildrenMode, int serializedVersionNumber, ref int bytesSoFar)
         {
+            base.ConvertFromBytesAfterHeader(OriginalIncludeChildrenMode, serializedVersionNumber, ref bytesSoFar);
             ReadOnlySpan<byte> span = LazinatorObjectBytes.Span;_MyListName = span.ToString_BrotliCompressedWithLength(ref bytesSoFar);
         }
         
@@ -114,8 +115,9 @@ namespace LazinatorTests.Examples
             _LazinatorObjectBytes = writer.Slice(startPosition);
         }
         
-        protected virtual void WritePropertiesIntoBuffer(BinaryBufferWriter writer, IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool includeUniqueID)
+        protected override void WritePropertiesIntoBuffer(BinaryBufferWriter writer, IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool includeUniqueID)
         {
+            base.WritePropertiesIntoBuffer(writer, includeChildrenMode, verifyCleanness, includeUniqueID);
             // write properties
             EncodeCharAndString.WriteBrotliCompressedWithIntPrefix(writer, _MyListName);
         }
