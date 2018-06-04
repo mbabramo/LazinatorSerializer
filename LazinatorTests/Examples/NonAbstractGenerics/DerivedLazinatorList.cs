0a8918a8bed0db58dd96166c2aa4bb53b8ca9839
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-04 13:46:45 -0400

    derived lazinator list works

--- LazinatorTests/Examples/NonAbstractGenerics/DerivedLazinatorList.cs
+++ LazinatorTests/Examples/NonAbstractGenerics/DerivedLazinatorList.cs
@@ -10,5 +10,16 @@ namespace LazinatorTests.Examples
 {
     public partial class DerivedLazinatorList<T> : LazinatorList<T>, IDerivedLazinatorList<T> where T : ILazinator, new()
     {
+        // We must override these methods, so that the code behind knows that they are defined in this class. Otherwise, it will not call them. 
+
+        public override void PreSerialization()
+        {
+            base.PreSerialization();
+        }
+
+        public override void MarkHierarchyClean()
+        {
+            base.MarkHierarchyClean();
+        }
     }
 }
