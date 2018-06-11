f42d793d9de94d54501af611c799cb4d0c42de29
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-11 08:42:07 -0400

    codegen 91

--- LazinatorAnalyzer/Roslyn/LazinatorVersionInfo.cs
+++ LazinatorAnalyzer/Roslyn/LazinatorVersionInfo.cs
@@ -7,7 +7,7 @@ namespace LazinatorCodeGen.Roslyn
     // NOTE: Must also change LazinatorVersionInfo in Lazinator project
     public static class LazinatorVersionInfo
     {
-        public static readonly Version LazinatorVersion = new Version(0, 1, 0, 90);
+        public static readonly Version LazinatorVersion = new Version(0, 1, 0, 91);
         public static readonly string LazinatorVersionString = LazinatorVersion.ToString();
         public static readonly byte[] LazinatorVersionBytes = Encoding.ASCII.GetBytes(LazinatorVersionString);
         public static int LazinatorIntVersion = 1; // this is encoded at the beginning of each Lazinator serialization
