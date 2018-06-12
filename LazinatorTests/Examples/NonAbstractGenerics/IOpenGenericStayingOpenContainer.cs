2bae5ac79f29a19eaf0d5b2019aea0bba119e147
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-12 14:33:43 -0400

    OpenGenericStayingOpenContainer updated

--- LazinatorTests/Examples/NonAbstractGenerics/IOpenGenericStayingOpenContainer.cs
+++ LazinatorTests/Examples/NonAbstractGenerics/IOpenGenericStayingOpenContainer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Lazinator.Attributes;
+using Lazinator.Core;
 using Lazinator.Wrappers;
 
 namespace LazinatorTests.Examples.NonAbstractGenerics
@@ -12,5 +13,6 @@ namespace LazinatorTests.Examples.NonAbstractGenerics
         // The reason this is more challenging than some other examples is that we haven't defined a new type, e.g. OpenGenericFloat, and then had ClosedGeneric be of that type. We are thus dependent on recording the type of the open generic.
         OpenGeneric<WFloat> ClosedGenericFloat { get; set; }
         OpenGeneric<IExampleChild> ClosedGenericInterface { get; set; }
+        OpenGeneric<IExampleNonexclusiveInterface> ClosedGenericNonexclusiveInterface { get; set; }
     }
 }
