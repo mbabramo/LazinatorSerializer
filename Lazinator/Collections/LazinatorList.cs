9251752f347796f544990f093ba21fe0d2278f77
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-12 16:33:36 -0400

    codegen

--- Lazinator/Collections/LazinatorList.cs
+++ Lazinator/Collections/LazinatorList.cs
@@ -155,7 +155,7 @@ namespace Lazinator.Collections
         {
             // this is the one change to the list (other than changes to specific indices) that does not require us to fully deserialize
             if (item?.LazinatorParentClass != null)
-                throw new MovedLazinatorException();
+                item = item.CloneLazinatorTyped();
             CreateUnderlyingListIfNecessary();
             ((IList<T>)UnderlyingList).Add(item);
             if (!FullyDeserialized)
@@ -203,7 +203,7 @@ namespace Lazinator.Collections
         public void Insert(int index, T item)
         {
             if (item?.LazinatorParentClass != null)
-                throw new MovedLazinatorException();
+                item = item.CloneLazinatorTyped();
             CreateUnderlyingListIfNecessary();
             ((IList<T>)UnderlyingList).Insert(index, item);
             if (!FullyDeserialized)
