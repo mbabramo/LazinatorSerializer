0c58e7337ed4ad7cf0fadfaddb4001ee04a08700
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-18 13:34:01 -0400

    dirtiness test

--- LazinatorTests/Tests/DirtinessTests.cs
+++ LazinatorTests/Tests/DirtinessTests.cs
@@ -7,6 +7,7 @@ using LazinatorTests.Examples.Collections;
 using Lazinator.Core;
 using Xunit;
 using LazinatorTests.Examples.Structs;
+using Lazinator.Wrappers;
 
 namespace LazinatorTests.Tests
 {
@@ -111,7 +112,75 @@ namespace LazinatorTests.Tests
         public void DirtinessEnumerationWorks()
         {
             var hierarchy = GetHierarchy(1, 1, 1, 1, 0);
-            var results = hierarchy.GetDirtyNodes(null, null, false).ToList();
+            var results = hierarchy.GetDirtyNodes().ToList();
+            results.Count().Should().BeGreaterThan(1);
+
+            var c = hierarchy.CloneLazinatorTyped();
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(0);
+
+            var accessed = c.MyChild1;
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(0);
+
+            c.MyUint = 12345;
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(1);
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(1); // still 1
+            c.LazinatorConvertToBytes();
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(0); // nothing is dirty now
+
+            c = c.CloneLazinatorTyped();
+            c.MyChild1 = new ExampleChild();
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(2); // parent and MyChild1
+
+            c = c.CloneLazinatorTyped();
+            c.MyChild1.MyLong = 565656;
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(1); // child only is dirty
+            (results[0] is ExampleChild).Should().BeTrue();
+
+            c = c.CloneLazinatorTyped();
+            c.MyChild1 = new ExampleChild();
+            c = c.CloneLazinatorTyped();
+            c.MyChild1 = new ExampleChild();
+            c.MyChild1.MyLong = 7878;
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(2); // now, parent and child again
+
+            c = c.CloneLazinatorTyped();
+            c.MyChild1.MyWrapperContainer = new WrapperContainer() { WrappedInt = 8 };
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(3); 
+            (results[1] is WrapperContainer).Should().BeTrue();
+            (results[2] is WInt).Should().BeTrue();
+
+            c = c.CloneLazinatorTyped();
+            c.MyChar = 'R';
+            c.MyChild1.MyWrapperContainer.WrappedInt = 9;
+            c.MyChild2 = new ExampleChild();
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(4);
+            (results[0] is Example).Should().BeTrue(); 
+            (results[1] is WrapperContainer).Should().BeTrue(); // wrapped int is a property of the container itself
+            (results[2] is WInt).Should().BeTrue();
+            (results[3] is ExampleChild).Should().BeTrue(); // i.e., MyChild2
+            results = c.GetDirtyNodes().ToList();
+            results.Count().Should().Be(4); // no change
+
+            results = c.GetDirtyNodes(null, null, true).ToList();
+            results.Count().Should().Be(1); // only highest dirty returned 
+            (results[0] is Example).Should().BeTrue();
+
+            results = c.GetDirtyNodes(x => !(x is WrapperContainer), null, false).ToList();
+            results.Count().Should().Be(3); // WrapperContainer is yielded but not further explored 
+            
+            results = c.GetDirtyNodes(null, x => x is WInt, false).ToList();
+            results.Count().Should().Be(1); // full exploration, but only WInt returned 
+            (results[0] is WInt).Should().BeTrue();
         }
 
     }
