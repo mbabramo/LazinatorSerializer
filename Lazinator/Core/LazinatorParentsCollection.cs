0633fcd6d48092d07fd8f5dd8ded7847d247bfe4
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-26 10:22:23 -0400

    LazinatorParentsCollectionWorks

--- Lazinator/Core/LazinatorParentsCollection.cs
+++ Lazinator/Core/LazinatorParentsCollection.cs
@@ -17,48 +17,84 @@ namespace Lazinator.Core
         // A complication is that an object can have the same parent more than once (that is, two childs of an
         // object can be the same), so we have to keep track of the number of times a parent is a parent.
 
-        private readonly Dictionary<ILazinator, byte> OtherParents;
+        private readonly Dictionary<ILazinator, int> OtherParents;
 
+        /// <summary>
+        /// The last Lazinator parent added. If the last added is subsequently removed, this will 
+        /// be null, even if some other parents were added earlier.
+        /// </summary>
         public readonly ILazinator LastAdded;
 
-        public bool Any() => LastAdded != null;
+        private readonly int LastAddedCount;
 
-        public int Count => LastAdded == null ? 0 : 1 + (OtherParents?.Sum(x => x.Value) ?? 0);
+        public bool Any() => LastAdded != null || (OtherParents != null && OtherParents.Any());
 
-        public LazinatorParentsCollection(ILazinator lastAdded, Dictionary<ILazinator, byte> otherParents = null)
+        /// <summary>
+        /// The number of parents stored. A parent is counted only once even if stored more often.
+        /// </summary>
+        public int Count => (LastAdded == null ? 0 : 1) + (OtherParents?.Count() ?? 0);
+
+        public LazinatorParentsCollection(ILazinator lastAdded, Dictionary<ILazinator, int> otherParents = null)
         {
             LastAdded = lastAdded;
+            LastAddedCount = (lastAdded == null) ? 0 : 1;
+            OtherParents = otherParents;
+        }
+
+        public LazinatorParentsCollection(ILazinator lastAdded, int lastAddedCount, Dictionary<ILazinator, int> otherParents = null)
+        {
+            LastAdded = lastAdded;
+            LastAddedCount = lastAddedCount;
             OtherParents = otherParents;
         }
 
         public LazinatorParentsCollection WithAdded(ILazinator parent)
         {
+            if (parent == null)
+                return this;
+
             if (LastAdded == null)
             {
+                if (OtherParents == null)
+                    return new LazinatorParentsCollection(parent);
+                if (OtherParents.ContainsKey(parent))
+                {   
+                    var otherParentsWithRemoval = OtherParents;
+                    int revisedCount = OtherParents[parent] + 1;
+                    otherParentsWithRemoval.Remove(parent);
+                    return new LazinatorParentsCollection(parent, revisedCount, otherParentsWithRemoval);
+                }
                 return new LazinatorParentsCollection(parent, OtherParents);
             }
-            var otherParents = OtherParents;
-            if (otherParents == null)
-                otherParents = new Dictionary<ILazinator, byte>();
-            if (otherParents.ContainsKey(parent))
-                otherParents[parent]++;
+
+            if (parent == LastAdded)
+                return new LazinatorParentsCollection(parent, LastAddedCount + 1, OtherParents);
+
+            // move LastAdded to OtherParents
+            var otherParentsWithAddition = OtherParents;
+            if (otherParentsWithAddition == null)
+                otherParentsWithAddition = new Dictionary<ILazinator, int>();
+            if (otherParentsWithAddition.ContainsKey(LastAdded))
+                otherParentsWithAddition[LastAdded]++;
             else
-                otherParents[parent] = 1;
-            return new LazinatorParentsCollection(LastAdded, otherParents);
+                otherParentsWithAddition[LastAdded] = 1;
+            return new LazinatorParentsCollection(parent, otherParentsWithAddition);
         }
 
         public LazinatorParentsCollection WithRemoved(ILazinator parent)
         {
             if (LastAdded == parent)
-            {
-                if (Count == 1)
+            { 
+                if (LastAddedCount > 1)
                 {
-                    return new LazinatorParentsCollection();
+                    return new LazinatorParentsCollection(parent, LastAddedCount - 1, OtherParents);
                 }
                 else
                     return new LazinatorParentsCollection(null, OtherParents);
             }
             var otherParents = OtherParents;
+            if (otherParents == null || !otherParents.ContainsKey(parent))
+                return this; // do not throw
             if (otherParents[parent] == 1)
                 otherParents.Remove(parent);
             else
@@ -75,6 +111,18 @@ namespace Lazinator.Core
                 {
                     if (parent.Key != LastAdded)
                         parent.Key.DescendantIsDirty = true;
+                }
+        }
+
+        public IEnumerable<ILazinator> EnumerateParents()
+        {
+            if (LastAdded != null)
+                yield return LastAdded;
+            if (OtherParents != null)
+                foreach (var parent in OtherParents)
+                {
+                    if (parent.Key != LastAdded)
+                        yield return parent.Key;
                 }
         }
     }
--- LazinatorTests/Tests/ParentsTests.cs
+++ LazinatorTests/Tests/ParentsTests.cs
@@ -15,6 +15,81 @@ namespace LazinatorTests.Tests
 {
     public class ParentsTests : SerializationDeserializationTestBase
     {
+        [Fact]
+        public void LazinatorParentsCollectionWorks()
+        {
+            ExampleChild e1 = new ExampleChild();
+            Example e2 = new Example();
+            ExampleChild e3 = new ExampleChild();
+
+            LazinatorParentsCollection c = new LazinatorParentsCollection();
+            c.Count.Should().Be(0);
+            c.Any().Should().BeFalse();
+            c.LastAdded.Should().BeNull();
+            c.EnumerateParents().Count().Should().Be(0);
+
+            c = c.WithAdded(e1);
+            c.Count.Should().Be(1);
+            c.Any().Should().BeTrue();
+            c.LastAdded.Should().Be(e1);
+            c.EnumerateParents().SequenceEqual(new ILazinator[] { e1 }).Should().BeTrue();
+
+            c = c.WithAdded(e1); // added a second time
+            c.Count.Should().Be(1);
+            c.Any().Should().BeTrue();
+            c.LastAdded.Should().Be(e1);
+            c.EnumerateParents().SequenceEqual(new ILazinator[] { e1 }).Should().BeTrue();
+
+            c = c.WithRemoved(e1);
+            c.Count.Should().Be(1);
+            c.Any().Should().BeTrue();
+            c.LastAdded.Should().Be(e1);
+            c.EnumerateParents().SequenceEqual(new ILazinator[] { e1 }).Should().BeTrue();
+
+            c = c.WithRemoved(e1);
+            c.Count.Should().Be(0);
+            c.Any().Should().BeFalse();
+            c.LastAdded.Should().BeNull();
+            c.EnumerateParents().Count().Should().Be(0);
+
+            c = c.WithAdded(e1).WithAdded(e2);
+            c.Count.Should().Be(2);
+            c.Any().Should().BeTrue();
+            c.LastAdded.Should().Be(e2);
+            c.EnumerateParents().SequenceEqual(new ILazinator[] { e2, e1 }).Should().BeTrue();
+
+            c = c.WithAdded(e3);
+            c.Count.Should().Be(3);
+            c.Any().Should().BeTrue();
+            c.LastAdded.Should().Be(e3);
+            c.EnumerateParents().Count().Should().Be(3);
+
+            c = c.WithRemoved(e3);
+            c.Count.Should().Be(2);
+            c.Any().Should().BeTrue();
+            c.LastAdded.Should().Be(null);
+            c.EnumerateParents().Count().Should().Be(2);
+
+            c = c.WithAdded(e2);
+            c.Count.Should().Be(2); // e2 only counts 1 time
+            c.Any().Should().BeTrue();
+            c.LastAdded.Should().Be(e2);
+            c.EnumerateParents().SequenceEqual(new ILazinator[] { e2, e1 }).Should().BeTrue();
+
+            c = c.WithRemoved(e2);
+            c.Count.Should().Be(2); // e2 only counts 1 time
+            c.Any().Should().BeTrue();
+            c.LastAdded.Should().Be(e2); // still listed as LastAdded
+            c.EnumerateParents().Count().Should().Be(2);
+
+            c = c.WithRemoved(e2).WithRemoved(e1);
+            c.Count.Should().Be(0);
+            c.Any().Should().BeFalse();
+            c.LastAdded.Should().Be(null);
+            c.EnumerateParents().Count().Should().Be(0);
+
+        }
+
 
         [Fact]
         public void SameObjectCanAppearTwice()
