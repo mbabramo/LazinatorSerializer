96df3e30dbc6d966e5418f04a3ec54b05c9db347
Michael Abramowicz <mbabramo@yahoo.com>
2018-06-28 16:44:58 -0400

    v.163 (main code only)

--- Lazinator/Support/LazinatorVersionInfo.cs
+++ Lazinator/Support/LazinatorVersionInfo.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 namespace Lazinator.Support
 {
-    // also change file in CodeGen with same class name
+    // also change file in analyzer project with same class name
     public static class LazinatorVersionInfo
     {
-        public static readonly Version LazinatorVersion = new Version(0, 1, 0, 162);
+        public static readonly Version LazinatorVersion = new Version(0, 1, 0, 163);
         public static readonly string LazinatorVersionString = LazinatorVersion.ToString();
         public static readonly byte[] LazinatorVersionBytes = Encoding.ASCII.GetBytes(LazinatorVersionString);
         public static int LazinatorIntVersion = 1; // this is encoded at the beginning of each Lazinator serialization
