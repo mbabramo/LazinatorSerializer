95b8bc0b17edb8fdb935250fd22d6c11bfd5fc5d
Michael Abramowicz <mbabramo@yahoo.com>
2018-07-26 13:52:07 -0400

    codegen

--- LazinatorTests/Examples/Collections/SpanAndMemory.cs
+++ LazinatorTests/Examples/Collections/SpanAndMemory.cs
@@ -4,6 +4,5 @@ namespace LazinatorTests.Examples.Collections
 {
     public partial class SpanAndMemory : ISpanAndMemory
     {
-        public ReadOnlyMemory<int>? MyNullableReadOnlyMemoryInt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
