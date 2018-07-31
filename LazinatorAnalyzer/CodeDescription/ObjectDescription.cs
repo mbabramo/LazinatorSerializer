e7bd174afe88d4591ddd97a95c1ff0fe0bc08342
Michael Abramowicz <mbabramo@yahoo.com>
2018-07-31 12:25:38 -0400

    CloneLazinator doesn't update stored buffer

--- LazinatorAnalyzer/CodeDescription/ObjectDescription.cs
+++ LazinatorAnalyzer/CodeDescription/ObjectDescription.cs
@@ -409,7 +409,7 @@ namespace Lazinator.CodeDescription
 
                         public {DerivationKeyword}ILazinator CloneLazinator(IncludeChildrenMode includeChildrenMode)
                         {{
-                            LazinatorMemory bytes = EncodeOrRecycleToNewBuffer(includeChildrenMode, OriginalIncludeChildrenMode, false, IsDirty, DescendantIsDirty, false, LazinatorObjectBytes, (EncodeManuallyDelegate)EncodeToNewBuffer, true /* DEBUG */);
+                            LazinatorMemory bytes = EncodeOrRecycleToNewBuffer(includeChildrenMode, OriginalIncludeChildrenMode, false, IsDirty, DescendantIsDirty, false, LazinatorObjectBytes, (EncodeManuallyDelegate)EncodeToNewBuffer, false);
                             var clone = new {NameIncludingGenerics}()
                             {{
                                 LazinatorParents = LazinatorParents,
