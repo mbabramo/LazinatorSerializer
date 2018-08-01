13a79ab49ca54b60da50c766309bf1309394bb4e
Michael Abramowicz <mbabramo@yahoo.com>
2018-08-01 07:26:44 -0400

    comment

--- Lazinator/Core/IncludeChildrenMode.cs
+++ Lazinator/Core/IncludeChildrenMode.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Lazinator.Core
 {
+    /// <summary>
+    /// Indication of which children should be serialized along with a parent Lazinator object. When a child is serialized, the mode will also apply to its children, recursively through the hierarchy.
+    /// </summary>
     public enum IncludeChildrenMode : byte
     {
         IncludeAllChildren,
