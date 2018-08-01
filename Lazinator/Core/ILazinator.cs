784ad07af297139408caee6ddeab9f4da2f67bea
Michael Abramowicz <mbabramo@yahoo.com>
2018-07-31 20:37:17 -0400

    comments cleaned up and organized

--- Lazinator/Core/ILazinator.cs
+++ Lazinator/Core/ILazinator.cs
@@ -10,28 +10,6 @@ namespace Lazinator.Core
     public interface ILazinator
     {
         /// <summary>
-        /// The memory (generally rented from a memory pool) used to initialize a Lazinator class/struct during deserialization. Header information, fields and child ISerializeds can then be read from this. This should be set when deserializing an object that represents the top of the hierarchy.
-        /// </summary>
-        LazinatorMemory HierarchyStorage { set; }
-        /// <summary>
-        /// The memory used to initialize a Lazinator class/struct during initial deserialization. Header information, fields and child ISerializeds can then be read from this. This is set automatically by the Lazinator framework, either from HierarchyStorage or from the parent's LazinatorObjectBytes.
-        /// </summary>
-        LazinatorMemory LazinatorMemoryStorage { get; set; }
-        /// <summary>
-        /// Returns true if the Lazinator object is a struct. This is used internally by Lazinator.
-        /// </summary>
-        bool IsStruct { get; }
-        /// <summary>
-        /// The parent (container) of the Lazinator class/struct, or null if there is none (because this is the top of the hierarchy or the parent is a struct or a class that doesn't implement ILazinator).
-        /// </summary>
-        LazinatorParentsCollection LazinatorParents { get; set; }
-
-        /// <summary>
-        /// Deserializes the class/struct and any serialized descendants from the object's serialized bytes. This is automatically called the first time there is an attempt to read a field or child, or when LazinatorObjectBytes is set. This reads data in the header and then calls ConvertFromBytesAfterHeader.
-        /// </summary>
-        /// <returns>The number of bytes processed during deserialization</returns>
-        int Deserialize();
-        /// <summary>
         /// Initiates serialization starting from here (and optionally including descendants), using the original bytes if the object is clean and manually writing bytes if necessary.
         /// </summary>
         /// <param name="includeChildrenMode">Whether child objects should be included. If false, the child objects will be skipped.</param>
@@ -39,14 +17,6 @@ namespace Lazinator.Core
         /// <param name="updateStoredBuffer">Whether the object being serialized should be updated to use the new buffer. This is ignored and treated as false if includeChildrenMode is not set to include all children. If false, then the returned memory will be wholly independent of the existing memory.</param>
         /// <returns></returns>
         LazinatorMemory SerializeLazinator(IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer);
-        /// <summary>
-        /// This is primarily used internally for communication between Lazinator objects. Continues serialization of this object and optionally its descendants by writing bytes into a pre-existing buffer. 
-        /// </summary>
-        /// <param name="writer">The BinaryBufferWriter to stream bytes to</param>
-        /// <param name="includeChildrenMode">Whether child objects should be included.  If false, the child objects will be skipped.</param>
-        /// <param name="verifyCleanness">Whether double-checking is needed to ensure that objects thought to be clean really are clean</param>
-        /// <param name="updateStoredBuffer">Whether the object being serialized should be updated to use the new buffer. This is ignored and treated as false if includeChildrenMode is not set to include all children.</param>
-        void SerializeExistingBuffer(ref BinaryBufferWriter writer, IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer);
         /// <summary>
         /// Clones the class/struct by serializing, including all children, and then deserializing.
         /// </summary>
@@ -59,23 +29,29 @@ namespace Lazinator.Core
         /// <param name="updateStoredBuffer">Whether to update the stored buffer, so that both copies have the same underlying buffer, assuming that all children are being included. If false, then the original object is unchanged.</param>
         /// <returns>A cloned copy of the class/struct</returns>
         ILazinator CloneLazinator(IncludeChildrenMode includeChildrenMode, bool updateStoredBuffer = false);
+
         /// <summary>
-        /// Indicates whether a Lazinator object has changed since it was last deserialized (or since the last call to EnsureLazinatorUpToDate).
+        /// Indicates whether a Lazinator object has changed since it was last deserialized (or since this property was manually changed).
         /// </summary>
         bool HasChanged { get; set; }
         /// <summary>
-        /// Indicates whether a descendant of a Lazinator object has changed since the object was last deserialized (or since the last call to MarkHierarchyClassesUnchanged).
+        /// Indicates whether a descendant of a Lazinator object has changed since the object was last deserialized (or since this property was manually changed).
         /// </summary>
         bool DescendantHasChanged { get; set; }
         /// <summary>
-        /// Indicates whether a Lazinator object is dirty, meaning that one of its fields has changed since it was last serialized. 
-        /// This may differ from HasChanged either if HasChanged is manually reset or if IsDirty is reset when the corresponding Lazinator object is serialized, for example during cloning.
+        /// Indicates that a Lazinator object is dirty, meaning that one of its properties has changed since it was last serialized. A Lazinator object is considered dirty when a non-Lazinator property is accessed, even if it is not changed, unless there is a boolean Dirty property for that non-Lazinator property. 
+        /// This may differ from HasChanged either if HasChanged is manually reset or if IsDirty is reset when the corresponding Lazinator object is serialized.
         /// </summary>
         bool IsDirty { get; set; }
         /// <summary>
         /// Indicates whether a descendant of a Lazinator object is dirty.
         /// </summary>
         bool DescendantIsDirty { get; set; }
+        /// <summary>
+        /// Internally serializes the Lazinator, if it has changed, and resets dirtiness properties (but not HasChanged). This does not need to be called manually before serialization.
+        /// </summary>
+        void EnsureLazinatorMemoryUpToDate();
+
         /// <summary>
         /// Enumerates nodes in the hierarchy based on specified parameters.
         /// </summary>
@@ -102,10 +78,6 @@ namespace Lazinator.Core
         IEnumerable<(string propertyName, object descendant)> EnumerateNonLazinatorProperties();
 
         /// <summary>
-        /// Converts the Lazinator object to a byte representation, if it has changed. This does not need to be called manually before serialization.
-        /// </summary>
-        void EnsureLazinatorMemoryUpToDate();
-        /// <summary>
         /// Returns the serialized length of an object, performing the serialization needed to make the computation if necessary.
         /// </summary>
         /// <returns></returns>
@@ -123,6 +95,36 @@ namespace Lazinator.Core
         /// </summary>
         Guid GetBinaryHashCode128();
 
+        
+        /// <summary>
+        /// This is primarily used internally for communication between Lazinator objects. Continues serialization of this object and optionally its descendants by writing bytes into a pre-existing buffer. 
+        /// </summary>
+        /// <param name="writer">The BinaryBufferWriter to stream bytes to</param>
+        /// <param name="includeChildrenMode">Whether child objects should be included.  If false, the child objects will be skipped.</param>
+        /// <param name="verifyCleanness">Whether double-checking is needed to ensure that objects thought to be clean really are clean</param>
+        /// <param name="updateStoredBuffer">Whether the object being serialized should be updated to use the new buffer. This is ignored and treated as false if includeChildrenMode is not set to include all children.</param>
+        void SerializeExistingBuffer(ref BinaryBufferWriter writer, IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer);
+        /// <summary>
+        /// Primarily used internally. Deserializes the class/struct and any serialized descendants from the object's serialized bytes. This is automatically called the first time there is an attempt to read a field or child, or when LazinatorObjectBytes is set. This reads data in the header and then calls ConvertFromBytesAfterHeader.
+        /// </summary>
+        /// <returns>The number of bytes processed during deserialization</returns>
+        int Deserialize();
+        /// <summary>
+        /// The memory (generally rented from a memory pool) used to initialize a Lazinator class/struct during deserialization. Header information, fields and child ISerializeds can then be read from this. This should be set when deserializing an object that represents the top of the hierarchy.
+        /// </summary>
+        LazinatorMemory HierarchyStorage { set; }
+        /// <summary>
+        /// The memory used to initialize a Lazinator class/struct during initial deserialization. Header information, fields and child ISerializeds can then be read from this. This is set automatically by the Lazinator framework, either from HierarchyStorage or from the parent's LazinatorObjectBytes.
+        /// </summary>
+        LazinatorMemory LazinatorMemoryStorage { get; set; }
+        /// <summary>
+        /// Returns true if the Lazinator object is a struct. This is used internally by Lazinator.
+        /// </summary>
+        bool IsStruct { get; }
+        /// <summary>
+        /// The parent (container) of the Lazinator class/struct, or null if there is none (because this is the top of the hierarchy or the parent is a struct or a class that doesn't implement ILazinator).
+        /// </summary>
+        LazinatorParentsCollection LazinatorParents { get; set; }
         /// <summary>
         /// The version number for this object.
         /// </summary>
