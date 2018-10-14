687c99493791883f5c32b7b3df4d426056927504
Michael Abramowicz <mbabramo@yahoo.com>
2018-10-14 09:59:39 -0400

    comments

--- Lazinator/Collections/LazinatorList.cs
+++ Lazinator/Collections/LazinatorList.cs
@@ -325,8 +325,7 @@ namespace Lazinator.Collections
                         var underlyingItem = UnderlyingList[itemIndex];
                         WriteChild(ref w, underlyingItem, includeChildrenMode, ItemHasBeenAccessed(itemIndex), () => GetListMemberSlice(itemIndex), verifyCleanness, updateStoredBuffer, false, true /* skip length altogether */, this);
                         if (underlyingItem != null && underlyingItem.IsStruct)
-                        { // the struct that was just written may be noted as dirty, but it's really clean. Cloning is the only safe way to get a clean hierarchy, because setting .IsDirty = false will not clear .IsDirty from nested structs.
-                            // DEBUG -- this marks as dirty.
+                        { // the struct that was just written may be noted as dirty, but it's really clean. Cloning is the only safe way to get a clean hierarchy.
                             underlyingItem = underlyingItem.CloneLazinatorTyped(IncludeChildrenMode.IncludeAllChildren, CloneBufferOptions.NoBuffer);
                             UnderlyingList[itemIndex] = underlyingItem;
                         }
