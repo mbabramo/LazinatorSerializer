c1b4eab5b8a7d924030ab722dfa64082cd8a3c00
Michael Abramowicz <mbabramo@yahoo.com>
2018-11-30 14:30:37 -0500

    add parens

--- LazinatorAnalyzer/CodeDescription/PropertyDescription.cs
+++ LazinatorAnalyzer/CodeDescription/PropertyDescription.cs
@@ -1358,7 +1358,7 @@ namespace Lazinator.CodeDescription
 
         private string EnsureExcludableChildrenLoaded()
         {
-            return $@"if (includeChildrenMode != IncludeChildrenMode.IncludeAllChildren || includeChildrenMode != OriginalIncludeChildrenMode && !_{PropertyName}_Accessed)
+            return $@"if ((includeChildrenMode != IncludeChildrenMode.IncludeAllChildren || includeChildrenMode != OriginalIncludeChildrenMode) && !_{PropertyName}_Accessed)
                                 {{
                                    var deserialized = {PropertyName};
                                 }}
