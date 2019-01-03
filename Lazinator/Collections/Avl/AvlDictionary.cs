6a4edcbb210e47992053041147b1f5b0b10916c4
Michael Abramowicz <mbabramo@yahoo.com>
2019-01-03 15:25:14 -0500

    AvlDictionary codegen

--- Lazinator/Collections/Avl/AvlDictionary.cs
+++ Lazinator/Collections/Avl/AvlDictionary.cs
@@ -21,9 +21,6 @@ namespace Lazinator.Collections.Avl
             AllowDuplicateKeys = allowDuplicates; // sometimes we allow multiple items with same key
         }
 
-
-
-
         public IEnumerable<TValue> GetAllValues(TKey key)
         {
             uint hash = key.GetBinaryHashCode32();
@@ -266,88 +263,6 @@ namespace Lazinator.Collections.Avl
         {
             return new Enumerator(UnderlyingTree.GetValueEnumerator());
         }
-
-
-        public AvlTree<WUint, LazinatorTuple<TKey, TValue>> UnderlyingTree { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool HasChanged { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool DescendantHasChanged { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsDirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool DescendantIsDirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public LazinatorMemory LazinatorMemoryStorage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool AllowDuplicateKeys { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public LazinatorMemory SerializeLazinator(IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeserializeLazinator(LazinatorMemory serialized)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ILazinator CloneLazinator(IncludeChildrenMode includeChildrenMode = IncludeChildrenMode.IncludeAllChildren, CloneBufferOptions cloneBufferOptions = CloneBufferOptions.IndependentBuffers)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateStoredBuffer()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void FreeInMemoryObjects()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<ILazinator> EnumerateLazinatorNodes(Func<ILazinator, bool> matchCriterion, bool stopExploringBelowMatch, Func<ILazinator, bool> exploreCriterion, bool exploreOnlyDeserializedChildren, bool enumerateNulls)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<(string propertyName, ILazinator descendant)> EnumerateLazinatorDescendants(Func<ILazinator, bool> matchCriterion, bool stopExploringBelowMatch, Func<ILazinator, bool> exploreCriterion, bool exploreOnlyDeserializedChildren, bool enumerateNulls)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<(string propertyName, object descendant)> EnumerateNonLazinatorProperties()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ILazinator ForEachLazinator(Func<ILazinator, ILazinator> changeFunc, bool exploreOnlyDeserializedChildren, bool changeThisLevel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetByteLength()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SerializeExistingBuffer(ref BinaryBufferWriter writer, IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateStoredBuffer(ref BinaryBufferWriter writer, int startPosition, int length, IncludeChildrenMode includeChildrenMode, bool updateDeserializedChildren)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ILazinator AssignCloneProperties(ILazinator clone, IncludeChildrenMode includeChildrenMode)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IncludeChildrenMode OriginalIncludeChildrenMode => throw new NotImplementedException();
-
-        public bool IsStruct => throw new NotImplementedException();
-
-        public bool NonBinaryHash32 => throw new NotImplementedException();
-
-        public LazinatorParentsCollection LazinatorParents { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int LazinatorObjectVersion { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
     }
 }
