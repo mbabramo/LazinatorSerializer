f4814dd68d0c285a3f038d7f82ecfcaf7d0cdc0b
Michael Abramowicz <mbabramo@yahoo.com>
2019-01-19 09:40:15 -0500

    partial codegen

--- Lazinator/Collections/Factories/ContainerFactory.cs
+++ Lazinator/Collections/Factories/ContainerFactory.cs
@@ -1,4 +1,5 @@
-﻿using Lazinator.Collections.Avl;
+﻿using Lazinator.Buffers;
+using Lazinator.Collections.Avl;
 using Lazinator.Collections.Avl.KeyValueTree;
 using Lazinator.Collections.Avl.ListTree;
 using Lazinator.Collections.Avl.ValueTree;
@@ -15,10 +16,28 @@ using System.Text;
 
 namespace Lazinator.Collections.Factories
 {
-    public partial class ContainerFactory<T> : IContainerFactory<T> where T : ILazinator
+    public partial class ContainerFactory<T> : IContainerFactory<T>, ILazinator where T : ILazinator
     {
         public ContainerLevel ThisLevel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public ContainerFactory<T> InteriorFactory { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool HasChanged { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DescendantHasChanged { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsDirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool DescendantIsDirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public LazinatorMemory LazinatorMemoryStorage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IncludeChildrenMode OriginalIncludeChildrenMode => throw new NotImplementedException();
+
+        public bool IsStruct => throw new NotImplementedException();
+
+        public bool NonBinaryHash32 => throw new NotImplementedException();
+
+        public LazinatorParentsCollection LazinatorParents { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int LazinatorObjectVersion { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ContainerFactory()
+        {
+        }
 
         public ContainerFactory(ContainerLevel thisLevel)
         {
@@ -153,6 +172,71 @@ namespace Lazinator.Collections.Factories
             {
                 return firstTree.GetApproximateDepth() < secondTree.GetApproximateDepth();
             }
+            throw new NotImplementedException();
+        }
+
+        public LazinatorMemory SerializeLazinator(IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeserializeLazinator(LazinatorMemory serialized)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILazinator CloneLazinator(IncludeChildrenMode includeChildrenMode = IncludeChildrenMode.IncludeAllChildren, CloneBufferOptions cloneBufferOptions = CloneBufferOptions.IndependentBuffers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStoredBuffer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FreeInMemoryObjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ILazinator> EnumerateLazinatorNodes(Func<ILazinator, bool> matchCriterion, bool stopExploringBelowMatch, Func<ILazinator, bool> exploreCriterion, bool exploreOnlyDeserializedChildren, bool enumerateNulls)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<(string propertyName, ILazinator descendant)> EnumerateLazinatorDescendants(Func<ILazinator, bool> matchCriterion, bool stopExploringBelowMatch, Func<ILazinator, bool> exploreCriterion, bool exploreOnlyDeserializedChildren, bool enumerateNulls)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<(string propertyName, object descendant)> EnumerateNonLazinatorProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILazinator ForEachLazinator(Func<ILazinator, ILazinator> changeFunc, bool exploreOnlyDeserializedChildren, bool changeThisLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetByteLength()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SerializeExistingBuffer(ref BinaryBufferWriter writer, IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStoredBuffer(ref BinaryBufferWriter writer, int startPosition, int length, IncludeChildrenMode includeChildrenMode, bool updateDeserializedChildren)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ILazinator AssignCloneProperties(ILazinator clone, IncludeChildrenMode includeChildrenMode)
+        {
             throw new NotImplementedException();
         }
     }
