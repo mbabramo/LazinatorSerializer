4077553af3f85c4302bf9658b8bdee9951962a35
Michael Abramowicz <mbabramo@yahoo.com>
2019-01-29 12:59:53 -0500

    codegen

--- LazinatorCollections/LazinatorQueue.cs
+++ LazinatorCollections/LazinatorQueue.cs
@@ -6,88 +6,6 @@ using System.Collections.Generic;
 
 namespace LazinatorCollections
 {
-    public partial class LazinatorQueue<T> : ILazinator
-    {
-        public bool HasChanged { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool DescendantHasChanged { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsDirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool DescendantIsDirty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public LazinatorMemory LazinatorMemoryStorage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public IncludeChildrenMode OriginalIncludeChildrenMode => throw new NotImplementedException();
-
-        public bool IsStruct => throw new NotImplementedException();
-
-        public bool NonBinaryHash32 => throw new NotImplementedException();
-
-        public LazinatorParentsCollection LazinatorParents { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int LazinatorObjectVersion { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public ILazinator AssignCloneProperties(ILazinator clone, IncludeChildrenMode includeChildrenMode)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ILazinator CloneLazinator(IncludeChildrenMode includeChildrenMode = IncludeChildrenMode.IncludeAllChildren, CloneBufferOptions cloneBufferOptions = CloneBufferOptions.IndependentBuffers)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeserializeLazinator(LazinatorMemory serialized)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<(string propertyName, ILazinator descendant)> EnumerateLazinatorDescendants(Func<ILazinator, bool> matchCriterion, bool stopExploringBelowMatch, Func<ILazinator, bool> exploreCriterion, bool exploreOnlyDeserializedChildren, bool enumerateNulls)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<ILazinator> EnumerateLazinatorNodes(Func<ILazinator, bool> matchCriterion, bool stopExploringBelowMatch, Func<ILazinator, bool> exploreCriterion, bool exploreOnlyDeserializedChildren, bool enumerateNulls)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<(string propertyName, object descendant)> EnumerateNonLazinatorProperties()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ILazinator ForEachLazinator(Func<ILazinator, ILazinator> changeFunc, bool exploreOnlyDeserializedChildren, bool changeThisLevel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void FreeInMemoryObjects()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int GetByteLength()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SerializeExistingBuffer(ref BinaryBufferWriter writer, IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public LazinatorMemory SerializeLazinator(IncludeChildrenMode includeChildrenMode, bool verifyCleanness, bool updateStoredBuffer)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateStoredBuffer()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateStoredBuffer(ref BinaryBufferWriter writer, int startPosition, int length, IncludeChildrenMode includeChildrenMode, bool updateDeserializedChildren)
-        {
-            throw new NotImplementedException();
-        }
-    }
     public partial class LazinatorQueue<T> : IEnumerable<T>, ILazinatorQueue<T> where T : ILazinator
     {
         public LazinatorQueue()
@@ -103,8 +21,6 @@ namespace LazinatorCollections
         public bool Any() => LongCount > 0;
         public int Count => UnderlyingList.Count;
         public long LongCount => UnderlyingList.LongCount;
-        
-        public ILazinatorListable<T> UnderlyingList { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public T Dequeue()
         {
