4ee11a08f29b8789933030c5e77888dd97d3f057
Michael Abramowicz <mbabramo@yahoo.com>
2019-01-29 13:32:04 -0500

    LazinatorConstructorEnum

---
+++ Lazinator/Core/LazinatorConstructorEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lazinator.Core
+{
+    /// <summary>
+    /// An enum that will be a parameter in the constructor that Lazinator calls automatically. A separate constructor is used so that if there is a parameterless constructor called by client code, it will not be called every time the object is deserialized. 
+    /// </summary>
+    public enum LazinatorConstructorEnum : byte
+    {
+        LazinatorConstructor
+    }
+}
