7ccf1b1ba1e1e4aa320013f7b6f5430bff559a32
Michael Abramowicz <mbabramo@yahoo.com>
2019-02-04 10:55:17 -0500

    InMemoryBlobStorage

--- LazinatorTests/Utilities/InMemoryBlobStorage.cs
+++ LazinatorTests/Utilities/InMemoryBlobStorage.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using Lazinator.Buffers;
+using Lazinator.Core;
+using Lazinator.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LazinatorTests.Utilities
 {
     public class InMemoryBlobStorage
     {
+        public Dictionary<string, Memory<byte>> Storage = new Dictionary<string, Memory<byte>>();
+
+        public Task<TValue> Get<TKey, TValue>(ILazinator key) where TKey : ILazinator where TValue : ILazinator
+        {
+            string stringKey = key.ToString();
+            if (Storage.ContainsKey(stringKey))
+            {
+                Memory<byte> memory = Storage[stringKey];
+                LazinatorMemory lazinatorMemory = new LazinatorMemory(memory);
+                TValue result;
+                try
+                {
+                    result = (TValue)DeserializationFactory.Instance.CreateFromBytesIncludingID(lazinatorMemory);
+                }
+                catch (InvalidCastException)
+                {
+                    throw new LazinatorDeserializationException($"Item stored at key {key} was not of expected type {typeof(TValue)}.");
+                }
+                return Task.FromResult(result);
+            }
+            return Task.FromResult(default(TValue));
+        }
+
+        public Task Set<TKey, TValue>(TKey key, TValue value) where TKey : ILazinator where TValue : ILazinator
+        {
+            string stringKey = key.ToString();
+            Memory<byte> memory = value.SerializeToArray();
+            Storage[stringKey] = memory;
+            return Task.CompletedTask;
+        }
     }
 }
