42fe33c4578733a59b2a6674e24f4d02d9b60f56
mbabramo <mbabramo@yahoo.com>
2020-08-14 10:44:44 -0400

    check for 0 length span

--- LazinatorAnalyzer/Support/CodeStringBuilder.cs
+++ LazinatorAnalyzer/Support/CodeStringBuilder.cs
@@ -9,7 +9,7 @@ namespace Lazinator.CodeDescription
         public int SpacesPerTab = 4;
         public int IndentLevel = 0;
         public bool IsBeginningOfLine = true;
-        public static bool AddLocationIndexComments = true; // DEBUG
+        public static bool AddLocationIndexComments = false;
         public static int LocationIndex = 0;
 
         public override string ToString()
