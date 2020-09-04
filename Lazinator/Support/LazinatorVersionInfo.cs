72edaa90262bf8d75d785eba8a2a1524a78aab33
mbabramo <mbabramo@yahoo.com>
2020-09-04 12:55:46 -0400

    v390

--- Lazinator/Support/LazinatorVersionInfo.cs
+++ Lazinator/Support/LazinatorVersionInfo.cs
@@ -8,7 +8,7 @@ namespace Lazinator.Support
     /// </summary>
     public static class LazinatorVersionInfo
     {
-        public static readonly Version LazinatorVersion = new Version(0, 1, 0, 389);
+        public static readonly Version LazinatorVersion = new Version(0, 1, 0, 390);
         public static readonly string LazinatorVersionString = LazinatorVersion.ToString();
         public static readonly byte[] LazinatorVersionBytes = Encoding.ASCII.GetBytes(LazinatorVersionString);
         public static int LazinatorIntVersion = 1; // this is encoded at the beginning of each Lazinator serialization
