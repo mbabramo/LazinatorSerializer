9cd9b39ec03326e763f490800a361cefff22324c
Michael Abramowicz <mbabramo@yahoo.com>
2020-09-30 10:21:27 -0400

    UncompressedContainer codegen

--- LazinatorTests/Examples/ExampleHierarchy/IUncompressedContainer.cs
+++ LazinatorTests/Examples/ExampleHierarchy/IUncompressedContainer.cs
@@ -6,24 +6,43 @@ namespace LazinatorTests.Examples.ExampleHierarchy
     [Lazinator((int) ExampleUniqueIDs.UncompressedContainer)]
     public interface IUncompressedContainer
     {
+        [Uncompressed]
         decimal MyUncompressedDecimal { get; set; }
+        [Uncompressed]
         short MyUncompressedShort { get; set; }
+        [Uncompressed]
         ushort MyUncompressedUShort { get; set; }
+        [Uncompressed]
         int MyUncompressedInt { get; set; }
+        [Uncompressed]
         uint MyUncompressedUInt { get; set; }
+        [Uncompressed]
         long MyUncompressedLong { get; set; }
+        [Uncompressed]
         ulong MyUncompressedULong { get; set; }
+        [Uncompressed]
         TimeSpan MyUncompressedTimeSpan { get; set; }
+        [Uncompressed]
         DateTime MyUncompressedDateTime { get; set; }
+        [Uncompressed]
         string MyUncompressed { get; set; }
+        [Uncompressed]
         decimal? MyUncompressedNullableDecimal { get; set; }
+        [Uncompressed]
         short? MyUncompressedNullableShort { get; set; }
+        [Uncompressed]
         ushort? MyUncompressedNullableUShort { get; set; }
+        [Uncompressed]
         int? MyUncompressedNullableInt { get; set; }
+        [Uncompressed]
         uint? MyUncompressedNullableUInt { get; set; }
+        [Uncompressed]
         long? MyUncompressedNullableLong { get; set; }
+        [Uncompressed]
         ulong? MyUncompressedNullableULong { get; set; }
+        [Uncompressed]
         TimeSpan? MyUncompressedNullableTimeSpan { get; set; }
+        [Uncompressed]
         DateTime? MyUncompressedNullableDateTime { get; set; }
     }
 }
--- LazinatorTests/Examples/ExampleHierarchy/UncompressedContainer.cs
+++ LazinatorTests/Examples/ExampleHierarchy/UncompressedContainer.cs
@@ -8,24 +8,5 @@ namespace LazinatorTests.Examples.ExampleHierarchy
 {
     public partial record UncompressedContainer : IUncompressedContainer
     {
-        public decimal MyUncompressedDecimal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public short MyUncompressedShort { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public ushort MyUncompressedUShort { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int MyUncompressedInt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public uint MyUncompressedUInt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public long MyUncompressedLong { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public ulong MyUncompressedULong { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public TimeSpan MyUncompressedTimeSpan { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public DateTime MyUncompressedDateTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string MyUncompressed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public decimal? MyUncompressedNullableDecimal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public short? MyUncompressedNullableShort { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public ushort? MyUncompressedNullableUShort { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int? MyUncompressedNullableInt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public uint? MyUncompressedNullableUInt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public long? MyUncompressedNullableLong { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public ulong? MyUncompressedNullableULong { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public TimeSpan? MyUncompressedNullableTimeSpan { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public DateTime? MyUncompressedNullableDateTime { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
