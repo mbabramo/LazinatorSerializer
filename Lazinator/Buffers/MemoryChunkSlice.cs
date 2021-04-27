953330e8d3497afa659d1d7287fc955c0d5edc66
mbabramo <mbabramo@yahoo.com>
2021-04-27 08:52:11 -0400

    MemoryChunkSlice

---
+++ Lazinator/Buffers/MemoryChunkSlice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazinator.Buffers
+{
+    public readonly struct MemoryChunkSlice
+    {
+        public readonly int AdditionalOffset;
+        public readonly int FinalLength;
+
+        public MemoryChunkSlice(int additionalOffset, int finalLength)
+        {
+            AdditionalOffset = additionalOffset;
+            FinalLength = finalLength;
+        }
+    }
+}
