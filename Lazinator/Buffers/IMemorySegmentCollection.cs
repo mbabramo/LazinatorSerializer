d531889cbabe8b75dcab1ee2fb9f8fec04a3d501
mbabramo <mbabramo@yahoo.com>
2021-05-04 08:01:34 -0400

    MemoryBlockSegmentByID, MemoryBlockSegmentByIndex

--- Lazinator/Buffers/IMemorySegmentCollection.cs
+++ Lazinator/Buffers/IMemorySegmentCollection.cs
@@ -6,6 +6,6 @@ namespace Lazinator.Buffers
     [Lazinator((int)LazinatorCoreUniqueIDs.IMemorySegmentCollection)]
     public interface IMemorySegmentCollection : IMemoryChunkCollection
     {
-        List<(int memoryBlockID, int offset, int length)> Segments { get; set; }
+        List<MemoryBlockSegmentByID> Segments { get; set; }
     }
 }
