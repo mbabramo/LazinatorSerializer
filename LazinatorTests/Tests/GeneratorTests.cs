3cc5c44c119cdc8c5fa033a31d339de7dca91112
mbabramo <mbabramo@yahoo.com>
2023-07-21 18:02:03 -0400

    tests modify

--- LazinatorTests/Tests/GeneratorTests.cs
+++ LazinatorTests/Tests/GeneratorTests.cs
@@ -104,7 +104,7 @@ namespace LazinatorTests.Tests
 
             bool exactMatch = filesGenerated[0].SourceText.ToString() == filesGeneratedInitially[0].SourceText.ToString(); // checks whether files match, including the date and version number.
             exactMatch.Should().BeFalse();
-            // DEBUG -- this isn't working. Why? PostGenerationInfo is being created both times, so it's not actually caching. But the new version doesn't appear here. Could it be something to do with what I'm calling. 
+            // DEBUG -- this isn't working. Why? PostGenerationInfo is being created both times, so it's not actually caching. But the new version doesn't appear here. Could it be something to do with what I'm calling. I think that one thing to do would be to actually create the source generator. 
 
             return Task.CompletedTask;
         }
