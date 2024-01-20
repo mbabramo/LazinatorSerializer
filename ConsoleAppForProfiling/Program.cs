f24676a5a14a097b850acfc63ad7e4b39edc99d3
mbabramo <mbabramo@yahoo.com>
2024-01-20 15:59:46 -0500

    add console app for profiling

---
+++ ConsoleAppForProfiling/Program.cs
@@ -0,0 +1,15 @@
+﻿using LazinatorTests.Tests;
+
+namespace ConsoleAppForProfiling
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            CodeGenTest t = new CodeGenTest();
+            await t.CodeGenerationProducesActualCode_CoreCollections();
+            await t.CodeGenerationProducesActualCode_Wrappers();
+            await t.CodeGenerationProducesActualCode_BuffersAndPersistence();
+        }
+    }
+}
