46ec9c6ce416fc6fe716acf4d5919950c8945499
mbabramo <mbabramo@yahoo.com>
2024-01-21 09:01:33 -0500

    add code to allow debugging source generator in console app

---
+++ ConsoleAppForProfiling/IMyLazinator.cs
@@ -0,0 +1,12 @@
+﻿using Lazinator.Attributes;
+
+namespace ConsoleAppForProfiling
+{
+    [Lazinator(1000)]
+    public interface IMyLazinator
+    {
+        int MyInt { get; set; }
+        string MyBool { get; set; }
+        List<int> MyList { get; set; }
+    }
+}
