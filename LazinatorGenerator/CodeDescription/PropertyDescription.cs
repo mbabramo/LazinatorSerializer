baf4e1369736e047603004f4449e58742d3843d9
mbabramo <mbabramo@yahoo.com>
2024-03-13 13:36:36 -0400

    remove change

--- LazinatorGenerator/CodeDescription/PropertyDescription.cs
+++ LazinatorGenerator/CodeDescription/PropertyDescription.cs
@@ -2021,7 +2021,7 @@ TabbedText.WriteLine($""{ILazinatorString} location: {{childData.ToLocationStrin
                             {{
                                 if ({innerProperty.GetNonNullCheck(false, "itemToClone[itemIndex]")})
                                 {{
-                                    itemToClone[itemIndex] = ({innerProperty.AppropriatelyQualifiedTypeName}) ((cloneOrChangeFunc(itemToClone[itemIndex]){innerProperty.PossibleUnsetExceptionWithForgivenessWithinNotNull}));
+                                    itemToClone[itemIndex] = ({innerProperty.AppropriatelyQualifiedTypeName}) ((cloneOrChangeFunc(itemToClone[itemIndex])));
                                 }}
                                 continue;
                             }}
